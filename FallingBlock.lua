80e21ced4f0fe64cc132f2e1cb2fad6e66de13b6
Tom1-3 <104271142+Tom1-3@users.noreply.github.com>
2022-09-02 20:50:53 +0100

    Update FallingBlock.lua

--- FallingBlock.lua
+++ FallingBlock.lua
@@ -5,22 +5,26 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 
-local Window = OrionLib:MakeWindow({Name = "Falling Color Block ", HidePremium = false, SaveConfig = true, ConfigFolder = "FallingBlockTroll"})
+local Window = OrionLib:MakeWindow({Name = "Falling Color Block ", HidePremium = false, SaveConfig = false, ConfigFolder = "OSH:FallingBlock"})
 
 
-OrionLib:MakeNotification({
+local Tab1 = Window:MakeTab({
 	Name = "Credits",
-	Content = "GUI By ThomasPlays! On Youtube",
-	Image = "rbxassetid://4483345998",
-	Time = 10
-})
-
-local Tab = Window:MakeTab({
-	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
+local Tab2 = Window:MakeTab({
+	Name = "Trolls",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Tab3 = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
 
 local chars = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
@@ -33,17 +37,18 @@ for i,v in pairs (game.Workspace:GetDescendants()) do
 end 
 end
 
-Tab:AddToggle({
+Tab2:AddToggle({
 	Name = "Touch All",
 	Default = false,
 	Callback = function(Value)
-		repeat
-		    Wait(1)
+		
+		 while Value == true do
+		    wait(.1)
 		    touchAll()
-		   until Value == false
+		  end
 	end    
 })
-Tab:AddSlider({
+Tab3:AddSlider({
 	Name = "Inf Jump Power",
 	Min = 50,
 	Max = 200,
@@ -57,13 +62,99 @@ Tab:AddSlider({
 })
 
 
-Tab:AddButton({
+Tab3:AddButton({
 	Name = "Infinite Jump",
 	Callback = function()
       	local Player = game:GetService'Players'.LocalPlayer;
 local UIS = game:GetService'UserInputService';
 
+Tab:AddBind({
+	Name = "Fly",
+	Default = Enum.KeyCode.G,
+	Hold = false,
+	Callback = function()
+		repeat wait() 
+	until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Head") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid") 
+local mouse = game.Players.LocalPlayer:GetMouse() 
+repeat wait() until mouse
+local plr = game.Players.LocalPlayer 
+local torso = plr.Character.Head 
+local flying = false
+local deb = true 
+local ctrl = {f = 0, b = 0, l = 0, r = 0} 
+local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+local maxspeed = 400 
+local speed = 5000 
 
+function Fly() 
+local bg = Instance.new("BodyGyro", torso) 
+bg.P = 9e4 
+bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
+bg.cframe = torso.CFrame 
+local bv = Instance.new("BodyVelocity", torso) 
+bv.velocity = Vector3.new(0,0.1,0) 
+bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
+repeat wait() 
+plr.Character.Humanoid.PlatformStand = true 
+if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
+speed = speed+.5+(speed/maxspeed) 
+if speed > maxspeed then 
+speed = maxspeed 
+end 
+elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
+speed = speed-1 
+if speed < 0 then 
+speed = 0 
+end 
+end 
+if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
+elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+else 
+bv.velocity = Vector3.new(0,0.1,0) 
+end 
+bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
+until not flying 
+ctrl = {f = 0, b = 0, l = 0, r = 0} 
+lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+speed = 0 
+bg:Destroy() 
+bv:Destroy() 
+plr.Character.Humanoid.PlatformStand = false 
+end 
+mouse.KeyDown:connect(function(key) 
+if key:lower() == "e" then 
+if flying then flying = false 
+else 
+flying = true 
+Fly() 
+end 
+elseif key:lower() == "w" then 
+ctrl.f = 1 
+elseif key:lower() == "s" then 
+ctrl.b = -1 
+elseif key:lower() == "a" then 
+ctrl.l = -1 
+elseif key:lower() == "d" then 
+ctrl.r = 1 
+end 
+end) 
+mouse.KeyUp:connect(function(key) 
+if key:lower() == "w" then 
+ctrl.f = 0 
+elseif key:lower() == "s" then 
+ctrl.b = 0 
+elseif key:lower() == "a" then 
+ctrl.l = 0 
+elseif key:lower() == "d" then 
+ctrl.r = 0 
+end 
+end)
+Fly()
+	end    
+})
 
 function Action(Object, Function) if Object ~= nil then Function(Object); end end
 
@@ -81,23 +172,8 @@ end)
   	end    
 })
 
-Tab:AddButton({
-	Name = "Credits",
-	Callback = function()
-	    OrionLib:MakeNotification({
-	Name = "Credits",
-	Content = "GUI By ThomasPlays! On Youtube",
-	Image = "rbxassetid://4483345998",
-	Time = 10
-})
-end   
-})
+Tab1:AddLabel("Made By Tom1-3 On Github")
 
-Tab:AddButton({
-	Name = "Destroy Script",
-	Callback = function()
-	  OrionLib:Destroy()
-end   
-})
+
 
 OrionLib:Init()
